984cf9fd137da4b5717cf2d09702909eb3126dfb
Abdulaki <147936105+abex-COM@users.noreply.github.com>
2024-02-19 14:28:00 +0300

    change all user interface and contents

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
+const body = document.querySelector('body');
 
-const input = document.querySelector('input');
-const btnc = document.querySelector('.but-c');
-// const numberInput = document.getElementById('numberInput');
-const result = document.querySelector('.result');
-const equal = document.querySelector('.equal')
-
-function buttonValues(value) {
-    input.value += value;
-}
-
-btnc.addEventListener(('click'), () => { input.value = '' });
+const bodyColor = document.querySelector('.body-color');
+const link = document.querySelector('.tel-link');
 
 
-equal.addEventListener(('click'), function () {
-    let numbers = input.value.split(',').map(Number).filter(num=>!isNaN(num) && num !=="");//split
-    let sum = numbers.reduce((total, num) => total + num, 0);//calculate sum
-    result.textContent = sum;
+bodyColor.addEventListener(('click'), function () {
+    if(body.style.getPropertyValue('background-color')==='rgb(204, 204, 204)'){
+        body.style.backgroundColor = 'white';
+  
+      }
+         else
+         {
+        body.style.backgroundColor = 'hsl(0,0%,80%)';
+  
+         }
 });
+
+
